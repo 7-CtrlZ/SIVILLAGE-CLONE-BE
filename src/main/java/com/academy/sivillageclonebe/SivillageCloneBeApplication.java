657b5254b05b577934ce934d98d1b857c8c332af
Your Name <you@example.com>
2024-08-28 15:17:15 +0900

    first

---
+++ src/main/java/com/academy/sivillageclonebe/SivillageCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.academy.sivillageclonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivillageCloneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SivillageCloneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/academy/sivillageclonebe/SivillageCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.sivillageclonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivillageCloneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
