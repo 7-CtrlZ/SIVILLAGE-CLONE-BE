c8a6ae41f500949ef89492d51accfab38ea55aed
Cualone <june568@naver.com>
2024-09-17 02:08:35 +0900

    Feat: 주문 생성 api

--- src/main/java/com/academy/sivillageclonebe/orders/controller/OrdersController.java
+++ src/main/java/com/academy/sivillageclonebe/orders/controller/OrdersController.java
@@ -1,7 +1,16 @@
 package com.academy.sivillageclonebe.orders.controller;
 
+import com.academy.sivillageclonebe.common.entity.CommonResponseEntity;
+import com.academy.sivillageclonebe.common.entity.CommonResponseMessage;
+import com.academy.sivillageclonebe.member.vo.SignUpRequestVo;
+import com.academy.sivillageclonebe.orders.service.OrdersService;
+import com.academy.sivillageclonebe.orders.vo.OrdersRequestVo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/member/orders")
 public class OrdersController {
+
+    private final OrdersService ordersService;
+
+    @Operation(summary = "SignUp API", description = "SignUp API 입니다.", tags = {"Auth"})
+    @PostMapping("/create")
+    public CommonResponseEntity<Void> createOrders(
+            @RequestBody OrdersRequestVo ordersRequestVo) {
+
+        ordersService.createOrder(ordersRequestVo.toDto());
+
+        return new CommonResponseEntity<>(
+                HttpStatus.OK,
+                CommonResponseMessage.SUCCESS.getMessage(),
+                null
+        );
+    }
 }
--- src/main/java/com/academy/sivillageclonebe/orders/repository/OrdersRepository.java
+++ src/main/java/com/academy/sivillageclonebe/orders/repository/OrdersRepository.java
@@ -1,7 +1,16 @@
 package com.academy.sivillageclonebe.orders.repository;
 
+import com.academy.sivillageclonebe.member.entity.Member;
 import com.academy.sivillageclonebe.orders.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> findByUuid(String uuid);
+
+    List<Orders> findAllByMemberUuid(String memberUuid);
+
+    void deleteByUuid(String uuid);
 }
