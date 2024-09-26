a466262bd67247db644148c887882b05215338d3
Cualone <june568@naver.com>
2024-09-26 16:59:53 +0900

    Feat: BaseResponse, ExceptionHandler 추가

---
+++ src/main/java/com/academy/sivillageclonebe/common/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.academy.sivillageclonebe.common.exception;
+
+
+import com.academy.sivillageclonebe.common.entity.BaseResponseStatus;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException{
+
+    private final BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        this.status = status;
+    }
+}
