70dbbdef909c255c42a93a2107181ed40095a84d
Cualone <june568@naver.com>
2024-09-27 11:11:26 +0900

    Feat: media 기능 구현

---
+++ src/main/java/com/academy/sivillageclonebe/media/application/MediaServiceImpl.java
@@ -0,0 +1,33 @@
+package com.academy.sivillageclonebe.media.application;
+
+
+import com.academy.sivillageclonebe.common.entity.BaseResponseStatus;
+import com.academy.sivillageclonebe.common.exception.BaseException;
+import com.academy.sivillageclonebe.media.dto.in.MediaRequestDto;
+import com.academy.sivillageclonebe.media.dto.out.MediaResponseDto;
+import com.academy.sivillageclonebe.media.infrastructure.MediaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MediaServiceImpl implements MediaService{
+
+    private final MediaRepository mediaRepository;
+
+    @Override
+    public MediaResponseDto addMedia(MediaRequestDto mediaRequestDto) {
+        return MediaResponseDto.from(mediaRepository.save(mediaRequestDto.toEntity()));
+    }
+
+    @Override
+    public void deleteMedia(Long mediaId) {
+        mediaRepository.deleteById(mediaId);
+    }
+
+    @Override
+    public MediaResponseDto getMedia(Long mediaId) {
+        return MediaResponseDto.from(mediaRepository.findById(mediaId)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NO_EXIST_MEDIA)));
+    }
+}
---
+++ src/main/java/com/academy/sivillageclonebe/media/vo/in/MediaRequestVo.java
@@ -0,0 +1,12 @@
+package com.academy.sivillageclonebe.media.vo.in;
+
+import lombok.Getter;
+
+@Getter
+public class MediaRequestVo {
+
+        private String mediaUrl;
+        private String mediaName;
+        private String mediaType;
+
+}
