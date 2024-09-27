98241179206890d42ca21d0796b3646d4771d34b
Cualone <june568@naver.com>
2024-09-27 11:12:02 +0900

    Revert "Feat: media 기능 구현"
    This reverts commit 70dbbdef909c255c42a93a2107181ed40095a84d.

--- src/main/java/com/academy/sivillageclonebe/media/infrastructure/MediaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.academy.sivillageclonebe.media.infrastructure;
-
-
-import com.academy.sivillageclonebe.media.domain.Media;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaRepository extends JpaRepository<Media, Long> {
-}
